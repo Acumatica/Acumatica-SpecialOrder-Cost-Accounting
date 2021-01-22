6735865ab2dc65e57b22076ed3a5b06c6bbcba42
dchhapgar <dchhapgar@acumatica.com>
2021-01-22 15:18:50 -0500

    * Enhancements: 1. Mark for PO to be checked for Special Order Item when added in Opportunity and Sales Quote 2. For Special Order Item, cost will be defaulted from opportunity to Sales Order.

---
+++ PX.SpecialOrderCostAccounting.Ext/CR/OpportunityMaintCostPXExt.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using PX.Data;
+using PX.Objects.CR;
+using PX.Objects.IN;
+using PX.Objects.SO;
+using static PX.Objects.CR.OpportunityMaint;
+
+namespace PX.SpecialOrderCostAccounting.Ext
+{
+    public class OpportunityMaintCostPXExt : PXGraphExtension<OpportunityMaint>
+    {
+        protected virtual void _(Events.FieldUpdated<CROpportunityProducts.inventoryID> e, PXFieldUpdated BaseInvoke)
+        {
+            if (BaseInvoke != null) { BaseInvoke(e.Cache, e.Args); }
+            if (e.Row == null) { return; }
+
+            InventoryItem item = (InventoryItem)PXSelectorAttribute.Select<CROpportunityProducts.inventoryID>(Base.Products.Cache, e.Row);
+            if (item == null) { return; }
+            InventoryItemCostPXExt itemExt = PXCache<InventoryItem>.GetExtension<InventoryItemCostPXExt>(item);
+
+            if (itemExt.UsrIsSpecialOrderItem.GetValueOrDefault(false))
+            {
+                e.Cache.SetValueExt<CROpportunityProducts.pOCreate>(e.Row, itemExt.UsrIsSpecialOrderItem);
+            }
+        }
+
+        public delegate void BaseDoCreateSalesOrder(CreateSalesOrderFilter param);
+
+        [PXOverride]
+        public virtual void DoCreateSalesOrder(CreateSalesOrderFilter param, BaseDoCreateSalesOrder BaseInvoke)
+        {
+            PXGraph.InstanceCreated.AddHandler<SOOrderEntry>((graph) =>
+            {
+                graph.RowUpdated.AddHandler<SOLine>((cache, eArgs) =>
+                {
+                    var soLine = (SOLine)eArgs.Row;
+                    InventoryItem item = (InventoryItem)PXSelectorAttribute.Select<SOLine.inventoryID>(cache, soLine);
+                    if (item != null)
+                    {
+                        InventoryItemCostPXExt itemExt = PXCache<InventoryItem>.GetExtension<InventoryItemCostPXExt>(item);
+                        if (itemExt.UsrIsSpecialOrderItem.GetValueOrDefault())
+                        {
+                            CROpportunityProducts oppLine = PXResult<CROpportunityProducts>.Current;
+                            cache.SetValueExt<SOLine.curyUnitCost>(soLine, oppLine.CuryUnitCost);
+                        }
+                    }
+                });
+            });
+
+            BaseInvoke(param);
+        }
+    }
+}
---
+++ PX.SpecialOrderCostAccounting.Ext/CR/QuoteMaintCostPXExt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using PX.Data;
+using PX.Objects.CR;
+using PX.Objects.IN;
+
+namespace PX.SpecialOrderCostAccounting.Ext
+{
+    public class QuoteMaintCostPXExt : PXGraphExtension<QuoteMaint>
+    {
+        protected virtual void _(Events.FieldUpdated<CROpportunityProducts.inventoryID> e, PXFieldUpdated BaseInvoke)
+        {
+            if (BaseInvoke != null) { BaseInvoke(e.Cache, e.Args); }
+            if (e.Row == null) { return; }
+
+            InventoryItem item = (InventoryItem)PXSelectorAttribute.Select<CROpportunityProducts.inventoryID>(Base.Products.Cache, e.Row);
+            if (item == null) { return; }
+            InventoryItemCostPXExt itemExt = PXCache<InventoryItem>.GetExtension<InventoryItemCostPXExt>(item);
+
+            if (itemExt.UsrIsSpecialOrderItem.GetValueOrDefault(false))
+            {
+                e.Cache.SetValueExt<CROpportunityProducts.pOCreate>(e.Row, itemExt.UsrIsSpecialOrderItem);
+            }
+        }
+    }
+}
