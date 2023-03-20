542bf88ca4e7e2fc20e54b92fb79cfb2c30625a0
DhirenChhapgar <dchhapgar@acumatica.com>
2023-03-20 18:35:27 -0400

    * Fixing issue with 0$ PO cost considering Avg cost for Inventory issue on Sales Invoice

--- PX.SpecialOrderCostAccounting.Ext/IN/INReleaseProcessCostPXExt.cs
+++ PX.SpecialOrderCostAccounting.Ext/IN/INReleaseProcessCostPXExt.cs
@@ -2,6 +2,7 @@
 using PX.Objects.IN;
 using PX.Objects.AR;
 using PX.Objects.SO;
+using PX.Objects.CM;
 
 namespace PX.SpecialOrderCostAccounting.Ext
 {
@@ -26,9 +27,54 @@ namespace PX.SpecialOrderCostAccounting.Ext
                 if (tranExt.UsrSpecialOrderCost.GetValueOrDefault(false))
                 {
                     layer.UnitCost = tran.UnitCost;
+                    if (PXCurrencyAttribute.IsNullOrEmpty(tran.UnitCost))
+                    {
+                        tran.TranCost = 0.00M;
+                    }
                 }
             }
             BaseInvoke(layer, tran, split, item, ref QtyUnCosted);
+
+            //Reset unitcost and trancost if zero
+            if ((PXCurrencyAttribute.IsNullOrEmpty(tran.UnitCost)) &&
+                (item.ValMethod == INValMethod.Average && itemExt?.UsrIsSpecialOrderItem == true &&
+                tran.DocType == INDocType.Issue && (tran.ARDocType == ARDocType.Invoice || tran.SOLineType == SOLineType.Inventory)))
+            {
+                INTranCostPXExt tranExt = PXCache<INTran>.GetExtension<INTranCostPXExt>(tran);
+                if (tranExt.UsrSpecialOrderCost.GetValueOrDefault(false))
+                {
+                    layer.UnitCost = tran.UnitCost;
+                    if (PXCurrencyAttribute.IsNullOrEmpty(tran.UnitCost))
+                    {
+                        tran.TranCost = 0.00M;
+                    }
+                }
+            }
+        }
+
+        public delegate void BaseTransferCost(INTran tran, INTranSplit split, InventoryItem item, INCostStatus issueCost, INTranCost issueTranCost, decimal issuedQty, decimal issuedCost);
+
+        [PXOverride]
+        public virtual void TransferCost(INTran tran, INTranSplit split, InventoryItem item, INCostStatus issueCost, 
+                                         INTranCost issueTranCost, decimal issuedQty, decimal issuedCost,
+                                         BaseTransferCost BaseInvoke)
+        {
+            InventoryItemCostPXExt itemExt = PXCache<InventoryItem>.GetExtension<InventoryItemCostPXExt>(item);
+            if ((PXCurrencyAttribute.IsNullOrEmpty(tran.UnitCost)) &&
+               (item.ValMethod == INValMethod.Average && itemExt?.UsrIsSpecialOrderItem == true &&
+               tran.DocType == INDocType.Issue && (tran.ARDocType == ARDocType.Invoice || tran.SOLineType == SOLineType.Inventory)))
+            {
+                INTranCostPXExt tranExt = PXCache<INTran>.GetExtension<INTranCostPXExt>(tran);
+                if (tranExt.UsrSpecialOrderCost.GetValueOrDefault(false))
+                {
+                    //Reverse accumulation
+                    issueCost.TotalCost += (issuedCost * -1);
+                    tran.TranCost = 0.00M;
+                    issueTranCost.TranCost = 0.00M;
+                }
+            }
+
+            BaseInvoke(tran, split, item, issueCost, issueTranCost, issuedQty, issuedCost);
         }
     }
 }
